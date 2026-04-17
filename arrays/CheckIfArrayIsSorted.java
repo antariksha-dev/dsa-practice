4741433340eb352f5976afe33218566f8c08b2fc
Antariksha Sharma <heyantariksha@gmail.com>
2026-04-17 22:55:27 +0000

    Check array is sorted

---
+++ arrays/CheckIfArrayIsSorted.java
@@ -0,0 +1,18 @@
+public class CheckIfArrayIsSorted {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        boolean result = isSorted(arr);
+        System.out.println(result);
+    }
+
+    public static boolean isSorted(int[] arr) {
+        for(int i=1; i<arr.length; i++) {
+            if(!(arr[i] > arr[i-1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

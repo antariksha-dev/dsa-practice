db171f92e19f243ffeaf3645e284233cdda5ddef
Antariksha Sharma <heyantariksha@gmail.com>
2026-04-19 07:48:22 +0000

    Two sum

---
+++ map/TwoSum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,4};
+        int target = 6;
+        int[] result = twoSum(nums, target);
+        System.out.println("Indices: " + Arrays.toString(result));
+    }
+
+    public static int[] twoSum(int[] arr, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<arr.length; i++) {
+            int complement = target - arr[i];
+            if(map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
+            }
+            map.put(arr[i], i);
+        }
+        return new int[] {-1, -1};
+    }
+}
